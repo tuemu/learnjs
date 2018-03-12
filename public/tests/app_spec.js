f6f51bfc4b215dbf74e5e2088a5b735af32d4a2b
Tomohiro Uemura <t.uemura618@gmail.com>
2018-03-13 00:11:36 +0800

    Until 2.3 in P38

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,16 +1,19 @@
-describe('LearnJS', function(){
-    it('can show a problem view', function(){
+describe('LearnJS', function() {
+    it('can show a problem view', function() {
         learnjs.showView('#problem-1');
-        expects($('.view container .problem-view').length).toEqual(1);
+        expect($('.view-container .problem-view').length).toEqual(1);
+    });
+    it('shows the landing page view when there is no hash', function(){
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
     });
 });
 
 /**
- *
- describe('LearnJS', function() {
-it('can show a problem view', function() {
-learnjs.showView('#problem-1');
-expect($('.view-container .problem-view').length).toEqual(1); });
+ it('shows the landing page view when there is no hash', function() { learnjs.showView('');
+expect($('.view-container .landing-view').length).toEqual(1);
 });
 
+ * 
+ * 
  */
