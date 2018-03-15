94c41820457ff505cd9d11b2f8d1c3d2d7e677ee
Tomohiro Uemura <t.uemura618@gmail.com>
2018-03-16 02:38:04 +0800

    Until 2.6 in P49

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -19,10 +19,33 @@ describe('LearnJS', function() {
             expect(view.text()).toEqual('Problem #1 Coming soon!');
         });
     });
+    it('invokes the router when loaded', function(){
+        spyOn(learnjs, 'showView');
+        learnjs.appOnReady();
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+    it('subscribes to the hash change event', function() {
+        learnjs.appOnReady();
+        spyOn(learnjs, 'showView');
+        $(window).trigger('hashchange');
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
     
 });
 
 /**
+it('subscribes to the hash change event', function() { learnjs.appOnReady();
+spyOn(learnjs, 'showView');
+$(window).trigger('hashchange'); expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+});
+
+it('invokes the router when loaded', function() {
+spyOn(learnjs, 'showView');
+learnjs.appOnReady(); expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+});
+
+
+ * 
 describe('problem view', function() {
 it('has a title that includes the problem number', function() {
 var view = learnjs.problemView('1');
