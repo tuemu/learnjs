1ab0b6b0e3214ef3fdcf1db834e7ca4217532cf4
Tomohiro Uemura <t.uemura618@gmail.com>
2018-04-14 02:15:20 +0800

    Until 3.3 in P64

--- public/app.js
+++ public/app.js
@@ -1,10 +1,58 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber) {
-    var title = 'Problem #' + problemNumber + ' Coming soon!';
+//source[learnjs/3100/prblic/app.js]{
+learnjs.problems = [
+    {
+        description: "What is truth?",
+        code: "function problem() {return __;}"
+    },
+    {
+        description: "Simple Math",
+        code: "function problem() {return 42 === 6 *  __;}"
+    }
+]
+//}
+
+// learnjs.problemView2 = function(problemNumber) {
+//     var title = 'Problem #' + problemNumber + ' Coming soon!';
+//     var view = $('.templates .problem-view').clone();
+//     view.find('.title').text(title);
+//     return view;
+// }
+/**
+learnjs.problemView = function(data) {
+var problemNumber = parseInt(data, 10);
+var view = $('.templates .problem-view').clone(); view.find('.title').text('Problem #' + problemNumber); 
+learnjs.applyObject(learnjs.problems[problemNumber 1], view); return view;
+}
+*/
+learnjs.problemView = function (data) {
+    var problemNumber = parseInt(data, 10);
     var view = $('.templates .problem-view').clone();
-    view.find('.title').text(title)
+    var problemData = learnjs.problems[problemNumber-1];
+    var resultFlash = view.find('.result');
+
+    function checkAnswer() {
+        console.log('checkAnswer');
+        var answer = view.find('.answer').val();
+        var test = problemData.code.replace('__', answer) + '; problem();';
+        console.log('test: '+ test);
+        return eval(test);
+    }
+
+    function checkAnswerClick() {
+        if (checkAnswer()) {
+            resultFlash.text('Correct');
+        } else {
+            resultFlash.text('Incorrect');
+        }
+        return false;
+    }
+
+    view.find('.check-btn').click(checkAnswerClick);
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(problemData, view);
     return view;
 }
 
@@ -13,7 +61,9 @@ learnjs.showView = function(hash) {
         '#problem': learnjs.problemView
     };
     var hashParts = hash.split('-');
+    //console.log('hashParts[0]: ' + hashParts[0]);
     var viewFn = routes[hashParts[0]];
+    //console.log(viewFn);
     if(viewFn) {
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
@@ -26,9 +76,16 @@ learnjs.appOnReady = function() {
     learnjs.showView(window.location.hash);
 }
 
+learnjs.applyObject = function(obj, elem) {
+    for (var key in obj) {
+        elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+}
+
 /**
-learnjs.problemView = function(problemNumber) {
-var view = $('.templates .problem-view').clone(); view.find('.title').text('Problem #' + problemNumber + ' Coming soon!'); return view;
+learnjs.problemView = function(data) {
+var problemNumber = parseInt(data, 10);
+var view = $('.templates .problem-view').clone(); view.find('.title').text('Problem #' + problemNumber); learnjs.applyObject(learnjs.problems[problemNumber 1], view); return view;
 }
 
 
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -14,10 +14,20 @@ describe('LearnJS', function() {
         expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
     describe('problem view', function(){
-        it('has a title that inculudes the problem number', function(){
+        it('has a title that includes the problem number', function(){
             var view = learnjs.problemView('1');
             expect(view.text()).toEqual('Problem #1 Coming soon!');
         });
+        it('has a title that includes the problem number', function() { 
+            expect(view.find('.title').text()).toEqual('Problem #1');
+        });
+        it('shows the description', function() { 
+            expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+        });
+        it('shows the problem code', function() {
+            expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
+        });
+    
     });
     it('invokes the router when loaded', function(){
         spyOn(learnjs, 'showView');
@@ -29,6 +39,19 @@ describe('LearnJS', function() {
         spyOn(learnjs, 'showView');
         $(window).trigger('hashchange');
         expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+    
+    describe('answer section', function() {
+        it('can check a correct answer by hitting a button', function() {
+            view.find('.answer').val('true'); 
+            view.find('.check-btn').click(); 
+            expect(view.find('.result').text()).toEqual('Correct!');
+        });
+        it('rejects an incorrect answer', function() { 
+            view.find('.answer').val('false'); 
+            view.find('.check-btn').click(); 
+            expect(view.find('.result').text()).toEqual('Incorrect!'); 
+        });
     });
     
 });
